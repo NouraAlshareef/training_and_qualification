2a183c452f33b0b0582e98866fa04943c37f99a3
NouraAlshareef <eng.nouraalshareef98@gmail.com>
2023-07-12 21:06:59 +0300

    Front-end Design

---
+++ training_and_qualification/training_and_qualification/doctype/training_field/test_training_field.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, NouraAlshareef and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTrainingField(FrappeTestCase):
+	pass
